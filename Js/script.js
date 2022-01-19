957558771a1c6e7b9a201901bf8d8df38eb78228
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-20 04:25:37 +0600

    solved my first ever JS problem, I'm so happy

--- Js/script.js
+++ Js/script.js
@@ -1,16 +1,5 @@
-// operators
-
-// arithmatic operators -> +,-,*,/,%
-// % -> this is called reminder, it prints the bhagshesh
-
-// increment decrement ->
-// a++ -> saves the value first then increments it
-// ++a -> increments the value first then saves it
-// a-- -> saves the value first then decrements it
-// --a -> decrements the value first then saves it
-
-
-// for arithmatic operators->
-// '/' -> * -> + -> - , works in this flow
-// if we want to highlight a part simply just add brackets
-// also '**' means exponential which is power , ex-> 2**3 = 8
+var c,d;
+c = prompt("what's the temperature?");
+d = 9/5*c + 32;
+window.alert("the temperature in ferenheit is->" + d);
+console.log("the task is complete");
