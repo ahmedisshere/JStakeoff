04d0eac890d5f0521e8d3a97e37a4a04255180d0
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-20 02:03:49 +0600

    script changes

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("A Legend is roaming on the field of JavaScript");
+console.log("The force has been connected");
+window.alert("Are you ready to join?");
+document.write("There is a Legend on the make");
