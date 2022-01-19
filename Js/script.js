24e02094d656986ac4c9237b3e53eb2eec7f1410
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-20 04:37:07 +0600

    problem solved

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,5 @@
 var c,d;
 c = prompt("what's the temperature?");
 d = 9/5*c + 32;
-window.alert("the temperature in ferenheit is->" + d);
-console.log("the task is complete");
+window.alert("the temperature in fahrenheit is-> " + d + " degree");
+console.log("the task is complete!");
