4e50b19130c2e78b8da487ce3d51a53ccf8f5b2e
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-21 02:56:09 +0600

    script changes

--- Js/script.js
+++ Js/script.js
@@ -3,3 +3,10 @@ c = prompt("what's the temperature?");
 d = 9/5*c + 32;
 window.alert("the temperature in fahrenheit is-> " + d + " degree");
 console.log("the task is complete!");
+
+
+// Methods
+
+// toUpperCase() -> makes characters capital
+// toLowerCase() -> makes characters small
+// trim() -> cuts off the spaces front and behind of a string
