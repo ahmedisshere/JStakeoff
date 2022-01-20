00030d647fa2e28aa76ac095ad027a9bd25140d7
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-21 03:26:36 +0600

    script changes

--- Js/script.js
+++ Js/script.js
@@ -12,4 +12,5 @@ console.log("the task is complete!");
 // trim() -> cuts off the spaces front and behind of a string
 // parseInt() -> prints as an integer number even the main variable is a float
 // parseFloat() -> prints the float 
-// slice(value as a parameter,value as a parameter) -> cuts a string into pieces, first value indicates where to start the cutting characters from and second one indicates that cut till 2nd value character
+// slice(1,5) -> starts the cut from the first parameter keeping it as it is, ends the cut before the 2nd parameter.
+// slice(-5,-1) -> starts the cut from the right side , opposite of the forward slice
