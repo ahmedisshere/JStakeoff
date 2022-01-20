0075f0e18ab8f06eed38d8ae6b440c19246131ef
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-21 04:06:26 +0600

    learning array

--- Js/methods.js
+++ Js/methods.js
@@ -10,3 +10,33 @@
 // .substr(1,5) -> starts cutting the string from the first parameter, and takes the rest 2nd parameter number of values
 // .replace(a,b) -> replaces characters of the 1st parameter with the 2nd parameter in a string 
 // .concat( x, "yolo" , "123") -> adds the value of the method as a string in an existing string as it is.
+
+
+
+// boolean
+// Boolean(x) -> this is how you declare the boolean of a var (if a variable has no value,if the value of a variable is null, or if a NaN , then the boolean output will be false)
+
+
+
+// ARRAY ->>
+
+// var countries = [ "Bangladesh" , "Canada" , "Germany" ] ;
+
+// countries[1]; -> this is how you print the selected item of an array.
+
+// countries[2] = "Greenland"; -> this is how you change a value of an array.
+
+// countries.length -> prints the value of the array, it always adds +1 to the last count of an array value.
+
+// countries[3] = "Norway" -> adds a new value to the last length of an array 
+
+// countries[countries.length] = "Finland" -> you will never know the size of an array in the backend of a site,so if you call this array method , 
+//                                            then it will always store the new string value in the last length location of an array 
+
+// countries.push("Sweden") -> adds an item to the last index of an array and prints the number of items in an array 
+
+// countries.pop() -> terminates an item from the end of an array an prints it 
+
+// countries.shift() -> terminates the first item of an array and prints it 
+
+// countries.unshift("Bangladesh") -> it adds an item to the first index of an array, or as a first item
--- Js/script.js
+++ Js/script.js
@@ -3,3 +3,7 @@ c = prompt("what's the temperature?");
 d = 9/5*c + 32;
 window.alert("the temperature in fahrenheit is-> " + d + " degree");
 console.log("the task is complete!");
+
+
+
+
