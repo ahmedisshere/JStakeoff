2bbee8bf73632cc1d94e9bd122787149eebff9a8
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-21 05:24:13 +0600

    Js objects and further

---
+++ Js/object.js
@@ -0,0 +1,17 @@
+// -->> Object declaration -->>
+
+// student ={name:"Superleggera" , age:25, hometown:"Dhaka"}; 
+
+// --> how to print objects -->>
+
+// student["age"]; Same as arrays in one way, will have to call the property name inside the bracket, then it will print the value.
+
+// student.age -> this will print the value of the object propert which is called after the dot.
+
+// student["Occupation"] = "student"; -> this will insert a new property and value to the object.
+
+// student.city = "Tangail"; -> this will also insert a new property and value to the object.
+
+// delete student.city; -> this will delete the property from the object and return a boolean value to the console
+
+// student = {}; ->> this will print/add an empty object 
