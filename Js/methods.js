3d2b8479425223931d359a3c09936f089f867ff5
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-21 03:34:46 +0600

    new script to reming me of lesson

---
+++ Js/methods.js
@@ -0,0 +1,12 @@
+// Methods
+
+// .toUpperCase() -> makes characters capital
+// .toLowerCase() -> makes characters small
+// .trim() -> cuts off the spaces front and behind of a string
+// .parseInt() -> prints as an integer number even the main variable is a float
+// .parseFloat() -> prints the float 
+// .slice(1,5) -> starts the cut from the first parameter keeping it as it is, ends the cut before the 2nd parameter.
+// .slice(-5,-1) -> starts the cut from the right side , opposite of the forward slice
+// .substr(1,5) -> starts cutting the string from the first parameter, and takes the rest 2nd parameter number of values
+// .replace(a,b) -> replaces characters of the 1st parameter with the 2nd parameter in a string 
+// .concat( x, "yolo" , "123") -> adds the value of the method as a string in an existing string as it is.
--- Js/script.js
+++ Js/script.js
@@ -3,14 +3,3 @@ c = prompt("what's the temperature?");
 d = 9/5*c + 32;
 window.alert("the temperature in fahrenheit is-> " + d + " degree");
 console.log("the task is complete!");
-
-
-// Methods
-
-// toUpperCase() -> makes characters capital
-// toLowerCase() -> makes characters small
-// trim() -> cuts off the spaces front and behind of a string
-// parseInt() -> prints as an integer number even the main variable is a float
-// parseFloat() -> prints the float 
-// slice(1,5) -> starts the cut from the first parameter keeping it as it is, ends the cut before the 2nd parameter.
-// slice(-5,-1) -> starts the cut from the right side , opposite of the forward slice
