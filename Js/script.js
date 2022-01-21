074aab20b9c4285d0483cda396cf9a999062abd3
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-22 05:17:08 +0600

    script changes

--- Js/script.js
+++ Js/script.js
@@ -2,8 +2,16 @@ var age = prompt();
 
 if(age >= 50){
     console.log("You're an OLD ass MF");
-}else{
-    console.log("You gotta grow");
 }
+else if(age >=30 && age < 50){
+    console.log("You can still do a lot in life!");
+}
+else{
+    console.log("You gotta grow tf up!");
+}
+
+
+
+
 
 console.log("TF you lookin at?");
