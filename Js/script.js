631f252c923886dce24be74af25ff5dd1cde3961
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-22 05:52:09 +0600

    Number Magic!

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,6 @@
-var n1 = prompt("First number: ");
-var n2 = prompt("Second number: ");
-var n3 = prompt("Third number: ");
+var n1 = prompt("First Number: ");
+var n2 = prompt("Second Number: ");
+var n3 = prompt("Third Number: ");
 
 n1 = parseInt(n1);
 n2 = parseInt(n2);
@@ -8,17 +8,17 @@ n3 = parseInt(n3);
 
 if (n1 >= n2){
     if(n1 >= n3){
-        document.write(n1 + " is the largest number!");
+        document.write(n1 + " is the largest Number!");
     }
     else{
-        document.write(n3 + " is the largest number!");
+        document.write(n3 + " is the largest Number!");
     }
 }
 else{
     if(n2 >= n3){
-        document.write(n2 + " Is the largest number!");
+        document.write(n2 + " Is the largest Number!");
     }
     else{
-        document.write(n3 + " Is the largest number!");
+        document.write(n3 + " Is the largest Number!");
     }
 }
