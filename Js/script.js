79d97c1a3f768adf0f721b791335818d9cd26462
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-23 05:12:12 +0600

    string and array iteration using loop

--- Js/script.js
+++ Js/script.js
@@ -1,8 +1,8 @@
-var a = 1;
+let ign = "I am kicking JavaScripts big ass butt!~";
+let food = ["Burger","Pizza","Hot Dawg","Kebab"];
+let len = food.length;
 
-for( a = 1 ; a <= 20 ; a++){
-    if( a % 2 == 1 ){
-        continue;
-    }
-    console.log(a);
+for(var i = 0 ; i < len ; i++){
+    console.log(`index : ${i}`);
+    console.log(food[i]);
 }
