f8619c8e2cf950255804d5b2473e2810d91b98f2
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-23 04:07:23 +0600

    while loop

--- Js/script.js
+++ Js/script.js
@@ -1,49 +1,16 @@
-console.log("Choose an Operation : \n1. Add \n2. Substract \n3. Multiply \n4. Divide");
+// While loop
 
-num1 = prompt("What's your first number?");
-num2 = prompt("What's your second number?");
-Ops = prompt("Choose an operation?");
+var i = 1;
+var sum = 0;
+var prod = 1;
 
-num1 = parseInt(num1);
-num2 = parseInt(num2);
-Ops = parseInt(Ops);
+while ( i <= 10 ){
 
-num1con = isNaN(num1);
-num2con = isNaN(num2);
-Opscon = isNaN(Ops);
+    sum = sum+i;
+    prod = prod * i;
+    i++;
 
-var result = null;
-
-if (num1con || num2con || Opscon) {
-    console.log("Invalid input");
-}
-else {
-    switch (Ops) {
-        case 1:
-            result = num1 + num2;
-            break;
-
-        case 2:
-            result = num1 - num2;
-            break;
-
-        case 3:
-            result = num1 * num2;
-            break;
-
-        case 4:
-            result = num1 / num2;
-            break;
-
-        default:
-            result = "WTF are you doin?";
-            break;
-    }
-    if (result == null) {
-        console.log("No Result!");
-    }
-    else {
-        console.log("Your Result is -> " + result);
-    }
 }
 
+console.log("Result -> " + sum);
+console.log("Prod result ->" + prod);
