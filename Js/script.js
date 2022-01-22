e36126069d38d2c3078010d77942b6f4c456ae5a
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-23 04:57:10 +0600

    break & continue

--- Js/script.js
+++ Js/script.js
@@ -1,35 +1,8 @@
-// While loop
+var a = 1;
 
-var i = 1;
-var sum = 0;
-var prod = 1;
-
-while ( i <= 10 ){
-
-    sum = sum+i;
-    prod = prod * i;
-    i++;
-
+for( a = 1 ; a <= 20 ; a++){
+    if( a % 2 == 1 ){
+        continue;
+    }
+    console.log(a);
 }
-
-console.log("Result -> " + sum);
-console.log("Prod result ->" + prod);
-console.log("End of while loop");
-console.log("Start of for loop");
-
-var j=0;
-
-for(j=1 ; j <= 10 ; j++){
-    console.log(j);
-}
-
-console.log("End of for loop");
-console.log("Start of reverse numbers loop");
-
-var k = 5;
-
-for(k = 5 ; k >= 1 ; k--){
-    console.log(k);
-}
-
-console.log("end of reverse for loop");
