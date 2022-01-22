454431e90c2529123e9dfcf2d4409c5d90cd4434
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-23 04:46:33 +0600

    loop and reverse loop

--- Js/script.js
+++ Js/script.js
@@ -14,3 +14,22 @@ while ( i <= 10 ){
 
 console.log("Result -> " + sum);
 console.log("Prod result ->" + prod);
+console.log("End of while loop");
+console.log("Start of for loop");
+
+var j=0;
+
+for(j=1 ; j <= 10 ; j++){
+    console.log(j);
+}
+
+console.log("End of for loop");
+console.log("Start of reverse numbers loop");
+
+var k = 5;
+
+for(k = 5 ; k >= 1 ; k--){
+    console.log(k);
+}
+
+console.log("end of reverse for loop");
