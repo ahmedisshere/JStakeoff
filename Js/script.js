3453af39fb09eab3902fa109ce4a5359d273937d
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-23 05:31:33 +0600

    for-in & for-of

--- Js/script.js
+++ Js/script.js
@@ -1,8 +1,23 @@
-let ign = "I am kicking JavaScripts big ass butt!~";
-let food = ["Burger","Pizza","Hot Dawg","Kebab"];
-let len = food.length;
+let ign = "I am kicking JavaScripts big ass butt!" ;
 
-for(var i = 0 ; i < len ; i++){
-    console.log(`index : ${i}`);
-    console.log(food[i]);
+let food = ["Burger","Pizza","Hot Dawg","Kebab"] ;
+
+let person = {
+    name: "Superleggera",
+    profession: "Counter Strike",
+    team: "Bangladesh",
+    age: 69
+}
+
+for(var x in food){
+    console.log(`Index  ${x} and item ${food[x]}`);
+}
+
+for(var y of food){
+    console.log(y);
+}
+
+
+for(var z in person){
+    console.log(`prop ${z} and value ${person[z]}`);
 }
