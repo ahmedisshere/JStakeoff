7296451fec08e8c16f16eb69edebfee2682c3790
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-23 03:39:19 +0600

    basic calcultor

--- Js/script.js
+++ Js/script.js
@@ -1,4 +1,4 @@
-console.log("Choose an Operation : \na. Add \nb. Substract \nc. Multiply \nd. Divide");
+console.log("Choose an Operation : \n1. Add \n2. Substract \n3. Multiply \n4. Divide");
 
 num1 = prompt("What's your first number?");
 num2 = prompt("What's your second number?");
@@ -6,29 +6,44 @@ Ops = prompt("Choose an operation?");
 
 num1 = parseInt(num1);
 num2 = parseInt(num2);
+Ops = parseInt(Ops);
+
+num1con = isNaN(num1);
+num2con = isNaN(num2);
+Opscon = isNaN(Ops);
 
 var result;
 
-switch (Ops) {
-    case "a" :
-        result = num1+num2;
-        break;
+if (num1con || num2con || Opscon) {
+    console.log("Invalid input");
+}
+else {
+    switch (Ops) {
+        case 1:
+            result = num1 + num2;
+            break;
 
-    case  "b" :
-        result = num1-num2;
-        break;
+        case 2:
+            result = num1 - num2;
+            break;
 
-    case  "c" :
-        result = num1*num2;
-        break;
+        case 3:
+            result = num1 * num2;
+            break;
 
-    case  "d" :
-        result = num1/num2;
-        break;
+        case 4:
+            result = num1 / num2;
+            break;
 
-    default:
-        result = "Invalid Input!";
-        break;
+        default:
+            result = "WTF are you doin?";
+            break;
+    }
+    if (result == null) {
+        console.log("No Result!");
+    }
+    else {
+        console.log("Your Result is -> " + result);
+    }
 }
 
-console.log("Your Result is -> " + result);
