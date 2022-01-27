584df7327639bd46d3b022962fd23640e122ec51
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-27 06:15:26 +0600

    forEach

--- Js/script.js
+++ Js/script.js
@@ -1,27 +1,10 @@
-// Normal expression
+// array iterating with foreach 
 
+var foods = ["Ice cream" , "chocolate" , "Cake" , "Pizza"];
 
+var numbers = [1,2,3,4];
 
-function saySomething(name1) {
-
-    console.log("Hello " + name1);
-}
-
-
-// Function expression -->>
-
-let saySomething1 = function (name2){
-    
-    console.log("Yo " + name2);
-}
-
-//Arrow function
-
-let saySomething2 = name3 => {
-
-    console.log("Waddup " + name3);
-}
-
-saySomething("Superleggeraa");
-saySomething1("cartman_");
-saySomething2("fatman_");
+foods.forEach(function(item,i,arr){
+    console.log( `index -> ${i} & item -> ${item}` );
+    console.log(arr);
+})
