e6eae715d97713bc8b4c4f11547eb27c3d4e10c2
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-27 06:29:19 +0600

    array mapping

--- Js/script.js
+++ Js/script.js
@@ -4,15 +4,17 @@ var foods = ["Ice cream" , "chocolate" , "Cake" , "Pizza"];
 
 var numbers = [1,2,3,4];
 
-// 1st parameter is for item
-// 2nd parameter is for index
-// 3rd parameter is for the whole array
 
-let printEverything = function(item,i,arr){
 
-    console.log( `index -> ${i} & item -> ${item}` );
+function foodMaping(item){
+
+    return ` ${item} is a food`;
 
 }
 
-foods.forEach(printEverything);
+
+let array_restore = foods.map(foodMaping);
+
+console.log(array_restore);
+
 
