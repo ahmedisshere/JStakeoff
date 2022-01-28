30fd9239bab69f3686a3fbb2a3a81dec4fa82a57
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-28 08:19:23 +0600

    learning Math Objects,these are some new fun fucntions

--- Js/script.js
+++ Js/script.js
@@ -1,18 +1,19 @@
-let friend = {
+// Math Objects ->>
 
-    firstname: "Tabib",
-    lastname: "Gondafi",
-    dob: "1-10-1996",
+let val;
 
-    fullname : function(){
-        return `${this.firstname} ${this.lastname}`;
-    }
-}
+val = Math.PI;                          // Prints the value of pi
+val = Math.E;                           // Prints a constant
+val = Math.round(1.9);                  // Prints the round integer 
+val = Math.ceil(1.2);                   // Prints the next round integer
+val = Math.floor(1.9);                  // Prints the previous round integer
+val = Math.sqrt(81);                    // Prints the square root of an integer
+val = Math.abs(-8);                     // Prints the absolute integer value of the variable 
+val = Math.pow(2,4);                    // Second parameter of the pow function behaves like the power of the first parameter
+val = Math.min(1,-2,9,-3,-9,14);        // Prints the lowest value of all the parameters
+val = Math.max(1,-2,9,-3,-9,14);        // Prints the higest value of all the parameters
+val = Math.random();                    // Generates a random number of decimal/floating points
+val = Math.random() * 100;              // This way the function generates a random number from min 0 to max the parameter
+val = Math.random() * 100 +1;           // This way you can generate a random number up to the parameters exact value
 
-console.log(friend.firstname);
-console.log(friend.fullname());
-
-
-let str = "superleggeraa";
-console.log(str.length);
-console.log(str.slice());
+console.log(val);
