10517ce7794f954fd425a0414ec9bd03bfe5b9ca
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-28 08:24:54 +0600

    Data function

--- Js/script.js
+++ Js/script.js
@@ -1,19 +1,8 @@
-// Math Objects ->>
+// Data function ->>
 
 let val;
+let today = new Date();
 
-val = Math.PI;                          // Prints the value of pi
-val = Math.E;                           // Prints a constant
-val = Math.round(1.9);                  // Prints the round integer 
-val = Math.ceil(1.2);                   // Prints the next round integer
-val = Math.floor(1.9);                  // Prints the previous round integer
-val = Math.sqrt(81);                    // Prints the square root of an integer
-val = Math.abs(-8);                     // Prints the absolute integer value of the variable 
-val = Math.pow(2,4);                    // Second parameter of the pow function behaves like the power of the first parameter
-val = Math.min(1,-2,9,-3,-9,14);        // Prints the lowest value of all the parameters
-val = Math.max(1,-2,9,-3,-9,14);        // Prints the higest value of all the parameters
-val = Math.random();                    // Generates a random number of decimal/floating points
-val = Math.random() * 100;              // This way the function generates a random number from min 0 to max the parameter
-val = Math.random() * 100 +1;           // This way you can generate a random number up to the parameters exact value
+val = today;
 
-console.log(val);
+console.log(today);
