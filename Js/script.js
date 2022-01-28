1df05c271a8645b56a540d0d038bb3272f9f77ad
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-28 08:48:14 +0600

    Learnt how to use date function, how to print todays details time using get functions, also how to change the value of a date function using set methods

--- Js/script.js
+++ Js/script.js
@@ -2,7 +2,30 @@
 
 let val;
 let today = new Date();
+let bDay = new Date('05-03-1996 12:05:37');
+bDay = new Date('May 03 1996');
+bDay = new Date('05/03/1996');
 
 val = today;
+val = today.toString();
 
-console.log(today);
+val = bDay;
+val = today.getMonth();
+val = today.getDate();
+val = today.getDay();
+val = today.getFullYear();
+val = today.getHours();
+val = today.getMinutes();
+val = today.getTime();     // Prints TimeStamp -> Timestamp is the amount of seconds past since 1st Januray 1970
+
+
+
+// console.log(val);
+
+bDay.setMonth(6);
+bDay.setDate(4);
+bDay.setFullYear(1969);
+bDay.setHours(2);
+bDay.setMinutes(54);
+
+console.log(bDay);
