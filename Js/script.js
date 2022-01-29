49ce6f634bd4383a9e420992b884ca5d10b11782
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-29 06:16:11 +0600

    getting ready for Object Oriented JavaScript

--- Js/script.js
+++ Js/script.js
@@ -4,39 +4,39 @@ var a = 1;
 let b = 2;
 const c = 3;
 
-console.log('global scope: ' , a, b, c );
+console.log('global scope: ', a, b, c);
 
-function test(){
+function test() {
 
     var a = 4;
     let b = 5;
-    const c =6;
-    console.log('function scope: ' , a,b,c);
+    const c = 6;
+    console.log('function scope: ', a, b, c);
 
 }
 
 test();
 
-console.log('global scope: ', a,b,c);
+console.log('global scope: ', a, b, c);
 
-if(true){
+if (true) {
 
     var a = 7;
     let b = 8;
     const c = 9;
-    console.log('if scope: ' ,a,b,c);
+    console.log('if scope: ', a, b, c);
 
 }
 
 
-console.log('global scope: ', a,b,c);
+console.log('global scope: ', a, b, c);
 
 
-for(var a = 0; a<=10; a++){
+for (var a = 0; a <= 10; a++) {
 
-    console.log('Loop scope :' , a);
+    console.log('Loop scope :', a);
 
 }
 
-console.log('global scope: ',a,b,c);
+console.log('global scope: ', a, b, c);
 
