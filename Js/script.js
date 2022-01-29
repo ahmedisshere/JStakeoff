dc9d74775659dcc7cfae088b27ef185e6eff59e8
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-29 06:09:17 +0600

    global / local scopes

--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,37 @@
-// var let const
+// var let const -> Global & Local scope
+
+var a = 1;
+let b = 2;
+const c = 3;
+
+console.log('global scope: ' , a, b, c );
+
+function test(){
+    var a = 4;
+    let b = 5;
+    const c =6;
+    console.log('function scope: ' , a,b,c);
+}
+
+test();
+
+console.log('global scope: ', a,b,c);
+
+if(true){
+
+    var a = 7;
+    let b = 8;
+    const c = 9;
+    console.log('if scope: ' ,a,b,c);
+
+}
+
+console.log('global scope: ', a,b,c);
+
+for(var a = 0; a<10; a++){
+    
+    console.log('Loop scope :' , a);
+}
+
+console.log('global scope: ',a,b,c);
+
