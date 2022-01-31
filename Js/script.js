31e12c802f5510226dc085f80fcdb564f184be64
Tausif Ahmed <tausifahmed472@gmail.com>
2022-01-31 07:18:20 +0600

    DOM started

--- Js/script.js
+++ Js/script.js
@@ -1,30 +1,43 @@
-// Static function
+let val;
 
-class Person {
+val = this;
+val = window;
+val = window.document;
+val = document;
+val = document.all;
+val = document.all.length;
+val = document.all[7];
+val = document.domain;
+val = document.URL;
+val = document.head;
+val = document.doctype;
 
-    constructor (fname,lname) {
-        
-        this.firstname = fname;
-        this.lastname = lname;
+val = document.forms;
+val = document.forms[0];
+val = document.forms[0].method;
 
-    }
+val = document.links;
+val = document.links[1];
+val = document.links[1].href;
+val = document.links[1].className;
 
-    greetings(){
+val = document.scripts;
+val = document.scripts[0];
+val = document.scripts[0].src;
+val = document.scripts[0].getAttribute('src');
 
-        console.log(`Hello ${this.firstname} ${this.lastname}`);
+val = document.links;
 
-    }
+links = document.links;
 
-    static test() {
+let linkArr = Array.from(links);
 
-        console.log("this is a static function");
+linkArr.forEach(function (link) {
 
-    }
+    console.log(link);
 
-}
+})
 
-let person1 = new Person("Tausif" , "Ahmed");
 
-console.log(person1.greetings());
-console.log(Person.test());
 
+console.log(val);
