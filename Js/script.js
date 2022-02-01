78fce8e0487bbeccf0bd5aa8cebf608b4965055c
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-01 10:30:33 +0600

    creating a new element in a list with js

--- Js/script.js
+++ Js/script.js
@@ -1,68 +1,21 @@
-// Traversing DOM
+// adding element to dom
 
-let val ;
+// creating elemet 
 
-let list = document.querySelector('ul');
+let olItem = document.createElement('li');
 
-let listItem = document.querySelector('ul li:first-child');
+olItem.className = 'new-class';
+olItem.id = 'newid';
 
+// adding attribute
 
-// getting child nodes
+olItem.setAttribute('title','a title to new element');
 
-val = list;
-val = list.childNodes;
-val = list.childNodes[1].nodeName;
-val = list.childNodes[0].nodeType; 
+// creating text elements ->
 
-// -->> nodetype returns ->
+olItem.appendChild( document.createTextNode('Next.js') );
 
-// 1 -> element
-// 2 -> attribute
-// 3 -> text node
-// 8 -> comment
-// 9 -> document itself
-// 10 -> doctype
+document.querySelector('ol').appendChild(olItem);
 
 
-val = list.children;
-
-val = list.children[0];
-
-list.children[0].textContent = 'babes';
-
-val = list.children[1].children[0];
-
-val = list.children[2].children[0].href;
-
-val = list.firstChild;
-
-val = list.firstElementChild;
-
-val = list.lastChild;
-
-val = list.lastElementChild;
-
-val = list.childElementCount;
-
-val = listItem;
-
-val = listItem.parentElement;
-
-val = listItem.parentNode;
-
-val = listItem.parentElement.parentElement;
-
-
-//
-
-
-val = listItem.nextElementSibling.nextElementSibling;
-
-//
-
-val = document.querySelector('ul li:last-child');
-
-val = val.previousElementSibling.previousElementSibling;
-
-console.log(val);
-
+console.log(olItem);
