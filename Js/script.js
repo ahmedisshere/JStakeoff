7ed306d217b8b9d827aa7fda638b6adb0963662c
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-01 11:53:06 +0600

    learned event listeners

--- Js/script.js
+++ Js/script.js
@@ -1,28 +1,9 @@
-// removing an element \
+// event listener
 
-let listItems = document.querySelectorAll('li');
+document.getElementById('btn').addEventListener('click',message);
 
-let list = document.querySelector('ul');
+function message() {
 
-// listItems[0].remove();
-// listItems[6].remove();
-
-// list.removeChild(listItems[5]);
-
-list.classList.add('test-class');
-list.classList.add('new-test');
-
-list.classList.remove('test-class');
-
-let val = list.hasAttribute('title');
-
-list.setAttribute('title','new title');
-list.setAttribute('bullshit','want some shit?');
-
-list.removeAttribute('bullshit');
-
-
-
-console.log(val);
-console.log(listItems);
-console.log(list);
+    console.log("Button clicked!");
+    
+}
