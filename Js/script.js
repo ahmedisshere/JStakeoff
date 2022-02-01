5df4af3b3ec6c932a79d672a5c6e407aaa91cb16
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-01 11:01:01 +0600

    ending the adding of element of lists

--- Js/script.js
+++ Js/script.js
@@ -11,13 +11,13 @@ olItem.id = 'newid';
 
 olItem.setAttribute('title','a title to new element');
 
-// creating text elements ->
+// creating new item in ordered list ->
 
 olItem.appendChild( document.createTextNode('Next.js') );
 
 document.querySelector('ol').appendChild(olItem);
 
-
+// New item in the unordered list ->
 
 let ulItem = document.createElement('li');
 let link = document.createElement('a');
