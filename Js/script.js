b392602871a88878af4123af8776be065c1333c8
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-03 07:12:42 +0600

    learning exec test match

--- Js/script.js
+++ Js/script.js
@@ -15,10 +15,20 @@ str = "again hello world";
 str = "asashelloasda world";
 str = "hello world";
 str = "again hello world hello";
+str = "hello world";
 
 // exec () - returns the result in an array or null.
 
 let result = re.exec(str);
+
+// test() ->  returns result in boolean -> true/false
+
+result = re.test(str);
+
+// match() -> returns value in a similar way like exec () -> but the declaration is reversed ->
+
+result = str.match(re);
+
 
 console.log(result);
 
