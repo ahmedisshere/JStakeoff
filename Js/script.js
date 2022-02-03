880bf050b58d6320521e85dddfa138f860ae1a50
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-03 06:41:13 +0600

    learned error handling

--- Js/script.js
+++ Js/script.js
@@ -7,16 +7,29 @@ try {
 
     test();
 
-}catch (err) {
+} catch (err) {
 
     console.log(err.message);
     console.log(err.name);
 
 }
-finally{
+finally {
 
     console.log("I'm inside finally");
-    
+
 }
 
 console.log("after test");
+
+let a = 20;
+
+try {
+
+    if (a < 20) throw "Too small";
+    else if (a > 25) throw "too big";
+
+} catch (err) {
+
+    console.log(err);
+
+}
