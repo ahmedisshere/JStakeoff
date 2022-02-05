987441852dbce51c15f2bf920bb3f5eda81c5ed6
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-05 08:05:21 +0600

    learning json data types

--- Js/script.js
+++ Js/script.js
@@ -1,15 +1,38 @@
+// jsonlint.com
+// JSON supports ->
+// string
+// Number
+// Object
+// Array
+// Boolean
+// Null
+
+//doesn't support undefined/function/date format
+
+
+
 var student = {
 
     name: "nabiha tahsin",
-    age: "72",
-    location: "Dhaka"
-
+    age: 72,
+    location: "Dhaka",
+    dob: "1999 - 01 - 01",
+    test_null: null,
+    test_undefined: undefined,
+    greet: function () {
+        console.log(`Hello ${this.name}`);
+    }
 };
 
-let student_json = JSON.stringify(student);
-
+var student_json = JSON.stringify(student);
 console.log(student_json);
 
-let student_new = JSON.parse(student_json);
 
-console.log(student_new);
+
+// let student_json = JSON.stringify(student);
+
+// console.log(student_json);
+
+// let student_new = JSON.parse(student_json);
+
+// console.log(student_new);
