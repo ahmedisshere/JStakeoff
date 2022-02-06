b74e8f17e6fc66ef8fe1e47b3347061397848725
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-06 09:04:22 +0600

    a chuck norris jokes generator

--- Js/script.js
+++ Js/script.js
@@ -1,44 +1,31 @@
-// using AJAX to fetch data without loading the page
-// AJAX = Asynchronous JavaScript and XML
-// Different kind of onload status -> 
-// 200 -> "OK", 403 = "Forbidden", 404 = "Not Found";
-// Different kind of ready state status ->
-// 0 -> "request not initialized", 1 -> "server connection established", 3 -> "request received", 4 -> "processing request" 5 -> "request finished and   process is ready"
+// https://www.icndb.com/api/
 
-document.getElementById('get_data').addEventListener('click', loadData);
+document.getElementById('get_data').addEventListener('click', loadJokes);
 
-function loadData() {
-
-    // creating XHR -> xml http request object 
+function loadJokes() {
 
     let xhr = new XMLHttpRequest();
 
-    xhr.open('GET', 'data.txt', true);
-
-    xhr.onprogress = function(){
-
-        console.log(xhr.readyState);
-
-    }
+    xhr.open('GET', 'http://api.icndb.com/jokes/random', true);
 
     xhr.onload = function () {
+
         if (this.status === 200) {
-            
-            document.getElementById('output').innerHTML = `<h2>${this.responseText}</h2>`;
-    
+
+            let data = JSON.parse(this.responseText);
+
+            let joke = data.value.joke;
+
+            console.log(joke);
+
+            document.getElementById('output').innerHTML = ` <h3> ${joke} </h3> `;
+
+
+
         }
     }
-    // Using onreadystatechange function to do the loading
 
-    // xhr.onreadystatechange = function(){
-
-    //     // console.log(this.readyState);
-
-    //     if(this.status===200 && this.readyState === 4){
-
-    //         console.log(this.readyState);
-    //     }
-    // }
     xhr.send();
-}
 
+
+}
