c012a58ac4cdde2547ec1f4a8db0aadac665fdf2
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-08 06:23:34 +0600

    understanding fetch api

--- Js/script.js
+++ Js/script.js
@@ -1,99 +1,14 @@
+// Initializing fetch api
+// fetch api uses javascript promises
 
-// Callback functions
-
-
-// let persons = [
-
-//     { firstName: "tausif", lastName: "Ahmed" },
-//     { firstName: "Nabiha", lastName: "Tahsin" }
-
-// ]
-
-// function createPerson(person) {
-
-//     setTimeout(function () {
-
-//         persons.push(person);
-
-//     }, 4000);
-// }
-
-// function getPerson() {
-
-//     setTimeout(function () {
-
-//         let output = '';
-
-//         persons.forEach(function (person) {
-
-//             output += `<li> ${person.firstName} ${person.lastName} </li>`
-
-//         });
-
-//         document.getElementById('output').innerHTML = output;
-
-//     }, 1000);
-// }
-
-// createPerson({firstName: "boomer", lastName: "game"});
-
-// getPerson();
+document.getElementById('get_data').addEventListener('click', getData);
 
 
+function getData() {
+    fetch('http://api.icndb.com/jokes/random')
+        .then(res => res.json())
 
-let persons = [
+        .then(data => { console.log(data.value.joke); })
 
-    { firstName: "John", lastName: "doe" },
-    { firstName: "Jane", lastName: "Doe" }
-
-]
-
-function createPerson(person) {
-
-    let prom = new Promise(function (resolve, reject) {
-        persons.push(person);
-
-        let error = false;
-
-        if (!error) {
-
-            resolve();
-
-        }
-        else {
-
-            reject(" Error! -> Something wrong --> go rewatch Error Handling ! ");
-
-        }
-
-        resolve();
-    });
-
-    return prom;
+        .catch(err => { console.log(err); })
 }
-
-function getPerson() {
-
-    setTimeout(function () {
-
-        let output = "";
-
-        persons.forEach(function (person) {
-
-            output += `<li> ${person.firstName} ${person.lastName} </li>`;
-
-        });
-
-        document.getElementById('output').innerHTML = output;
-
-    }, 500);
-}
-
-createPerson({ firstName: "boomer", lastName: "game" }).then(getPerson).catch(function (err) {
-
-    console.log(err);
-
-});
-
-
-
