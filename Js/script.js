460fe2b8c6ef53bead9d1ab4959ee03cba65cd62
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-08 06:24:15 +0600

    need to learn arrow function again

--- Js/script.js
+++ Js/script.js
@@ -5,10 +5,13 @@ document.getElementById('get_data').addEventListener('click', getData);
 
 
 function getData() {
+
     fetch('http://api.icndb.com/jokes/random')
+
         .then(res => res.json())
 
         .then(data => { console.log(data.value.joke); })
 
         .catch(err => { console.log(err); })
+        
 }
