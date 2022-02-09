17d496db872ceaf6187dacb6f5c9dc728c3cc600
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-09 07:23:04 +0600

    debugging js project

--- Js/script.js
+++ Js/script.js
@@ -1,2 +1,30 @@
 // Project JS
 // Defining UI elements
+
+let form = document.querySelector('#task_form');
+let taskList = document.querySelector('ul');
+let clearBtn = document.querySelector('#clear_task_btn');
+let filter = document.querySelector('#task_filter');
+let taskInput = document.querySelector('#new_task');
+
+form.addEventListener('submit', addTask);
+
+//defining functions 
+// adding a task
+
+function addTask(e) {
+
+    if (taskInput.value === '') {
+
+        alert('Add a Task?');
+
+    }
+    else{
+        // creating an Li element
+
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(taskInput.value + " "));
+        taskList.appendChild(li);
+        // taskInput.value = '';
+    }
+}
