c22037391ae7a88a1705c92c64f1f187c958045b
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-09 08:32:27 +0600

    creating a local storage for the project

--- Js/script.js
+++ Js/script.js
@@ -12,6 +12,9 @@ let taskInput = document.querySelector('#new_task');
 
 
 form.addEventListener('submit', addTask);
+taskList.addEventListener('click', removeTask);
+clearBtn.addEventListener('click', clearTask);
+filter.addEventListener('keyup', filterTask);
 
 //defining functions ->
 // adding a task ->
@@ -22,19 +25,78 @@ function addTask(e) {
 
         alert('Add a Task?');
 
-    } else{
+    } else {
         // creating an Li element
 
         let li = document.createElement('li');
         li.appendChild(document.createTextNode(taskInput.value + " "));
 
         let link = document.createElement('a');
-        link.setAttribute('href','#');
+        link.setAttribute('href', '#');
         link.innerHTML = 'x';
         li.appendChild(link);
 
         taskList.appendChild(li);
-        taskInput.value = '' ;
+
+        storeTastInLocalStorage(taskInput.value);
+        
+        taskInput.value = '';
     }
     e.preventDefault();
+}
+
+
+// Removing a task ->
+
+
+function removeTask(e) {
+
+    if (e.target.hasAttribute("href")) {
+
+        if (confirm("Are you sure?")) {
+
+            let element = e.target.parentElement;
+            element.remove();
+        }
+    }
+}
+
+
+// Clearing the Task List ->
+
+
+function clearTask(e) {
+
+    // taskList.innerHTML = " "; -->> this can do the work too by clearing      the task list
+
+    // Faster way -> 
+    while (taskList.firstChild) {
+
+        taskList.removeChild(taskList.firstChild);
+
+    }
+}
+
+
+
+// Filtering task ->>
+
+function filterTask(e) {
+
+    let text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('li').forEach(task => {
+
+        let item = task.firstChild.textContent;
+
+        if (item.toLowerCase().indexOf(text) != -1) {
+
+            task.style.display = 'block';
+
+        }
+        else {
+            task.style.display = 'none';
+
+        }
+    });
 }
