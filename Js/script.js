e372fb9068a0ad0b1af6305319ef1412c05184a4
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-10 06:32:18 +0600

    solved a bug

---
+++ Js/script.js
@@ -0,0 +1,68 @@
+// Getting the UI elements ->
+
+let form = document.querySelector('#book-form');
+
+
+
+// Book Class ->
+
+class Book {
+    constructor(title, author, isbn){
+
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+
+    }
+}
+
+
+// UI class, which will add the submissions in the table -> 
+
+class UI {
+    constructor(){
+
+    }
+    addToBookList(book){
+
+        let list = document.querySelector('#book-list');
+        let row = document.createElement('tr');
+
+        row.innerHTML = `<td> ${book.title} </td>
+        <td> ${book.author} </td>
+        <td> ${book.isbn} </td>
+        <td> <a href="#" class="delete"> X </a> </td>`;
+
+        list.appendChild(row);
+    }
+
+    clearFields(){
+
+    }
+
+}
+
+
+// Adding event listener ->
+
+form.addEventListener('submit', newBook);
+
+
+// Defining functions ->
+
+
+function newBook(e){
+    
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let isbn = document.querySelector("#isbn").value; 
+
+    let book = new Book(title,author,isbn);
+    let ui = new UI;
+    ui.addToBookList(book);
+
+    ui.clearFields();
+
+    e.preventDefault();
+
+}
