a460d6ce9cb271335e5fd735948d6fc7bf1d31d6
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-13 06:21:26 +0600

    mapping with arrow functions recap

--- Js/script.js
+++ Js/script.js
@@ -14,3 +14,11 @@ let numbers = [1,2,3,4,5,6];
 let printAll = item => console.log(item);
 
 numbers.forEach(printAll);
+
+
+
+
+let squareAll = item => item*item;
+let squareNum = numbers.map(squareAll);
+
+console.log(squareNum);
