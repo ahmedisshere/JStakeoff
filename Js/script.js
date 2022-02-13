cfb7a585ca9185050c8a4a4332be2cb3a2d234c5
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-14 04:21:42 +0600

    learned how to print the maximum number using the math.max function with spread operator

--- Js/script.js
+++ Js/script.js
@@ -1,30 +1,5 @@
-// Getting started with spread operator -> ... (Which is three dots)
+// Spread Operator with function ->
 
-let str = "markeloff";
+let numbers = [69,23,1,9,0,-69];
 
-let newStr = [...str];
-
-// console.log(newStr);
-
-let fruit1 = ["mango", "grape", "apple"];
-let fruit2 = ["banana", "orange"];
-let newfruit = "jackfruit";
-
-let newArr = [newfruit, ...fruit1, ...fruit2];
-
-console.log(newArr);
-
-
-// Spread operator on Objects (ES8 feature) ->
-
-let person = {
-
-    fname: "cartman",
-    lname: "fatman"
-
-};
-
-
-let newPerson = { ...person, dob: "03-05-1996" };
-
-console.log(newPerson);
+console.log(Math.max(...numbers));
