d6ee8288f8b19255e6339e22a7e6e5ce8e7e443e
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-13 06:54:44 +0600

    understood array destructing

--- Js/script.js
+++ Js/script.js
@@ -1,24 +1,7 @@
-// Arrow functions details ->
-// curly braces can be avoided when there is only one statement
-// also if there's any return statement then the curly braces cant be avoided
+// array destructing ->
 
-let getSquare = num =>  num*num; // This way we can minify an arrow function
+let fruits = ["apple","mango","grape"];
 
-// console.log(getSquare(4));
+let [,fruit2,fruit3] = fruits;
 
-
-// callback functions with foreach ->
-
-let numbers = [1,2,3,4,5,6,7,8];
-
-let printAll = item => console.log(item);
-
-numbers.forEach(printAll);
-
-
-
-
-let squareAll = item => item*item;
-let squareNum = numbers.map(squareAll);
-
-console.log(squareNum);
+console.log(fruit2,fruit3);
