042df0bfb701c5da946f24613bc6e4f728f39788
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-14 04:27:21 +0600

    learned spread operator with functions and arrays

--- Js/script.js
+++ Js/script.js
@@ -3,3 +3,15 @@
 let numbers = [69,23,1,9,0,-69];
 
 console.log(Math.max(...numbers));
+
+
+let gamer = ["cartman","fatman","markeloff"];
+
+let test = (fname,mname,lname) => {
+
+    console.log(`Hello ${fname} ${mname} ${lname}`);
+};
+
+test(gamer[0],gamer[1],gamer[2]); // this can be simplified by spread operator ->
+
+test(...gamer);  // <- Like this
