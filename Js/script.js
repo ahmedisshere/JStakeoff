5fb420c069bcd4cb8c823e07b6bff0295ba2dceb
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-13 06:40:13 +0600

    template literals recap

--- Js/script.js
+++ Js/script.js
@@ -9,7 +9,7 @@ let getSquare = num =>  num*num; // This way we can minify an arrow function
 
 // callback functions with foreach ->
 
-let numbers = [1,2,3,4,5,6,7];
+let numbers = [1,2,3,4,5,6,7,8];
 
 let printAll = item => console.log(item);
 
