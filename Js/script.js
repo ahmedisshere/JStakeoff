ae8bf441f4ee3b76b057600aa0e4b6c9fccf95d7
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-13 06:16:18 +0600

    arrow functions seems fun today, need this motivation everyday

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,16 @@
 // Arrow functions details ->
 // curly braces can be avoided when there is only one statement
+// also if there's any return statement then the curly braces cant be avoided
 
-let getSquare = num => {return num*num;}
+let getSquare = num =>  num*num; // This way we can minify an arrow function
 
-console.log(getSquare(4));
+// console.log(getSquare(4));
+
+
+// callback functions with foreach ->
+
+let numbers = [1,2,3,4,5,6];
+
+let printAll = item => console.log(item);
+
+numbers.forEach(printAll);
