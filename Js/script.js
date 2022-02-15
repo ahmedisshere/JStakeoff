6e6f696334fc1284c2213cb6c784ac974083366b
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-16 05:55:05 +0600

    iterator and generator done

--- Js/script.js
+++ Js/script.js
@@ -1,33 +1,54 @@
 // Iterator ->
 
-let iterable = "fatman";
+// let iterable = "fatman";
 
-iterable = [1,2,3,4,5,6];
+// iterable = [1, 2, 3, 4, 5, 6];
 
-let iter = iterable[Symbol.iterator]();
+// let iter = iterable[Symbol.iterator]();
 
-// Lets make a custom iterator ->
+// // Lets make a custom iterator ->
 
-let ign = ["cartman","fatman","markeloff","movemint"];
+// let ign = ["cartman", "fatman", "markeloff", "movemint"];
 
-function customIterator (arr) {
+// function customIterator(arr) {
 
-    let i = 0;
+//     let i = 0;
 
-    return {
-        next: function(){
+//     return {
+//         next: function () {
 
-            return i < arr.length ? { value: arr[i++], done: false} : {value: undefined, done: true};
-        }
-    };
-};
+//             return i < arr.length ? { value: arr[i++], done: false } : { value: undefined, done: true };
+//         }
+//     };
+// };
 
-let gamers = customIterator(ign);
+// let gamers = customIterator(ign);
 
-console.log(gamers.next().value);
-console.log(gamers.next().value);
-console.log("random bullshit ...");
-console.log(gamers.next().value);
-console.log(gamers.next().value);
-console.log(gamers.next());
+// console.log(gamers.next().value);
+// console.log(gamers.next().value);
+// console.log("random bullshit ...");
+// console.log(gamers.next().value);
+// console.log(gamers.next().value);
+// console.log(gamers.next());
 
+// Custome generator -->>
+
+function* genFunction() {
+
+    yield 1;
+    console.log("Go moo your neighbours lawn");
+    yield "fatman";
+    console.log("Go moo your neighbours lawn");
+    console.log("Go moo your neighbours lawn");
+    yield 2;
+    yield "movemint";
+    yield 69;
+    yield "bullshit";
+}
+
+let iter = genFunction();
+
+console.log(iter.next().value);
+console.log(iter.next().value);
+console.log(iter.next().value);
+console.log(iter.next().value);
