62e929fb2873e72ba4268719d57304376eb93060
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-16 05:16:57 +0600

    starting iterator

--- Js/script.js
+++ Js/script.js
@@ -11,7 +11,7 @@ console.log(iter.next().value);
 console.log(iter.next().value);
 console.log(iter.next().value);
 
-console.log("some other bullshit....");
+console.log("some other bullshit goes here....");
 
 console.log(iter.next().value);
 console.log(iter.next().value);
