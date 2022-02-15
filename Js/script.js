e6fb535b22862bb7e916fe24f6fa4257690216d4
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-16 05:58:38 +0600

    generator

--- Js/script.js
+++ Js/script.js
@@ -1,36 +1,3 @@
-// Iterator ->
-
-// let iterable = "fatman";
-
-// iterable = [1, 2, 3, 4, 5, 6];
-
-// let iter = iterable[Symbol.iterator]();
-
-// // Lets make a custom iterator ->
-
-// let ign = ["cartman", "fatman", "markeloff", "movemint"];
-
-// function customIterator(arr) {
-
-//     let i = 0;
-
-//     return {
-//         next: function () {
-
-//             return i < arr.length ? { value: arr[i++], done: false } : { value: undefined, done: true };
-//         }
-//     };
-// };
-
-// let gamers = customIterator(ign);
-
-// console.log(gamers.next().value);
-// console.log(gamers.next().value);
-// console.log("random bullshit ...");
-// console.log(gamers.next().value);
-// console.log(gamers.next().value);
-// console.log(gamers.next());
-
 // Custome generator -->>
 
 function* genFunction() {
