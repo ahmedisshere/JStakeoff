c78633ab8289fbbf9d1b4b88564da1740fa750e2
Tausif Ahmed <tausifahmed472@gmail.com>
2022-02-16 07:44:57 +0600

    map,array map,converting map to array

--- Js/script.js
+++ Js/script.js
@@ -19,6 +19,17 @@ myMap.set("third key", 69);
 
 // foreach -> generally in foreach loop, first parameter passes value and second parameter passes index, But in MAP first parameter passes value and second parameter passes KEY**
 
-myMap.forEach((x,y)=>{    // -> foreach works differently for map parameter wise
-    console.log(x,y);
-})
+// myMap.forEach((x,y)=>{    // -> foreach works differently for map parameter wise
+//     console.log(x,y);
+// })
+
+
+// -->> CONVERTING MAP TO ARRAY -->>
+
+// let arr = Array.from(myMap); -> converting
+
+// let arr = Array.from(myMap.keys()); // -> getting just the keys in the array
+
+let arr = Array.from(myMap.values()); // -> getting the valus 
+
+console.log(arr);
